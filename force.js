2949b94540a30d15a56ac813679c67343ae92b71
gaskaj <joegaska@gmail.com>
2018-03-20 11:10:30 -0400

    Added Stringify

--- force.js
+++ force.js
@@ -173,7 +173,7 @@ module.exports = function (RED) {
                 if (targetType == 'string') {
                     payload = JSON.stringify(payload);
                 } else {
-                    payload = JSON.parse(payload);
+                    payload = JSON.parse(JSON.stringify(payload));
                 }
             }
         }catch(err){
